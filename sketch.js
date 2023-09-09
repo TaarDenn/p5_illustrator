571f1cb2e25b742e03313094ce06392688fc4dc1
mac <mac@macs-MacBook-Pro.local>
2023-09-10 01:33:01 +0330

    fix snapping

--- sketch.js
+++ sketch.js
@@ -3,37 +3,64 @@
 // Instagram: @taardenn
 // Reddit: u/taardenn
 
-let supArr = [];
+let drawing = [];
 let snap = false;
-let snapDist = 50;
+let snapDist = 25;
 let activePoint;
 
+let input;
+let img;
 
 function setup() {
   createCanvas(400, 400);
-  colorMode(HSB, 360, 100, 100, 100)
+  colorMode(HSB, 360, 100, 100, 100);
   strokeWeight(2);
+  noFill()
+
+  // Init
+  input = createFileInput(handleFile);
+  input.position(0, 0);
+  drawing.push([]);
 }
 
 function draw() {
-  background(32, 5, 95);
-  activePoint = snap ? nearestPoint(snapDist) : createVector(mouseX, mouseY)
-  drawLines()
+  if (img) {
+    image(img, 0, 0, width, height);
+  }
+  background(32, 5, 95, 50);
+  activePoint = snap ? nearestPoint(snapDist) : new P(mouseX, mouseY);
+  drawLines();
+}
+
+function handleFile(file) {
+  if (file.type === "image") {
+    print(file);
+    img = createImg(file.data, "");
+    img.hide();
+    if (drawing[drawing.length - 1].length == 0) {
+      drawing.pop();
+    }
+  } else {
+    img = null;
+    if (drawing[drawing.length - 1].length == 0) {
+      drawing.pop();
+    }
+  }
 }
 
 function drawLines() {
-  if (supArr.length != 0) {
-
-    supArr.forEach((arr) => {
-      for (let i = 0; i < arr.length - 1; i++) {
-        line(arr[i].x, arr[i].y, arr[i + 1].x, arr[i + 1].y)
+  if (drawing.length != 0) {
+    drawing.forEach((shape) => {
+      for (let i = 0; i < shape.length - 1; i++) {
+        line(shape[i].x, shape[i].y, shape[i + 1].x, shape[i + 1].y);
       }
     });
 
-    let arr = supArr[supArr.length - 1]
-    if (arr.length != 0) {
-      line(arr[arr.length - 1].x,
-        arr[arr.length - 1].y,
+    let shape = drawing[drawing.length - 1];
+    if (shape.length != 0) {
+      line(
+        shape[shape.length - 1].x,
+        shape[shape.length - 1].y,
         activePoint.x,
         activePoint.y
       );
@@ -42,79 +69,84 @@ function drawLines() {
 }
 
 function nearestPoint(_maxDist) {
-  let refDist = _maxDist;
-  let np;
-  supArr.forEach((arr, i) => {
-    let dists = arr.map(p => dist(p.x, p.y, mouseX, mouseY));
-    let minDist = Math.min(...dists);
-    if (minDist < refDist) {
-      np = snappedPoint = supArr[i][dists.indexOf(minDist)];
-      push()
-      stroke([0,100,100])
-      strokeWeight(8)
-      point(np.x, np.y)
-      pop()
-    }
-  });
-  if (!np) {
-    np = snappedPoint = createVector(mouseX, mouseY);
-  }
+  const nps = drawing
+    .flat()
+    .filter((p) => dist(p.x, p.y, mouseX, mouseY) < _maxDist);
+  if (nps.length == 0) return new P(mouseX, mouseY);
+
+  const np = nps.reduce((a, b) =>
+    dist(a.x, a.y, mouseX, mouseY) < dist(b.x, b.y, mouseX, mouseY) ? a : b
+  );
+
+  // Don't snap on last point
+  if (
+    drawing[drawing.length - 1].length != 0 &&
+    np.x ==
+      drawing[drawing.length - 1][drawing[drawing.length - 1].length - 1].x &&
+    np.y ==
+      drawing[drawing.length - 1][drawing[drawing.length - 1].length - 1].y
+  )
+    return new P(mouseX, mouseY);
+
+  // Styling snapped point
+  push();
+  stroke([0, 100, 100]);
+  strokeWeight(1);
+  circle(np.x, np.y, 8);
+  pop();
+
   return np;
 }
 
 function mouseReleased() {
-  let p = snap ? snappedPoint : createVector(mouseX, mouseY)
-  // First time click
-  if (supArr.length == 0) {
-    let arr = [];
-    supArr.push(arr);
-  }
-  supArr[supArr.length - 1].push(p);
+  drawing[drawing.length - 1].push(activePoint);
 }
 
 function keyReleased() {
-  if (supArr.length == 0) {
+  // Press RETURN or ENTER to end shape
+  if (keyCode === RETURN || keyCode === ENTER) {
+    let arr = [];
+    drawing.push(arr);
     return;
   }
 
-  // RETURN or ENTER to end shape
-  if (keyCode === RETURN || keyCode === ENTER ) {
-    let arr = [];
-    supArr.push(arr);
-    return false;
-  }
-
+  // Deactive snap mode by releasing "S"
   if (keyCode == 83) {
     snap = false;
   }
+
+  // Press "U" to undo
   if (keyCode == 85) {
-    if (supArr[supArr.length - 1].length == 0) {
-      supArr.pop()
+    if (drawing[drawing.length - 1].length == 0) {
+      drawing.pop();
     }
-    supArr[supArr.length - 1].pop()
+    drawing[drawing.length - 1].pop();
   }
-  
-  // E to export shapes
+
+  // Press "E" to export shapes
   if (keyCode === 69) {
-    let str = '['
-    supArr.forEach((arr) => {
-      str = str + '['
-      arr.forEach((p) => {
-        str = str + '[' + p.x + ', ' + p.y + '],'
-      })
-      str = str.slice(0, -1) + '],'
-    })
-    str = str.slice(0, -3) + ']';
-    let output = splitTokens(str, ']],')
-    saveStrings(output, 'str.txt');
+    if (drawing[drawing.length - 1].length == 0) {
+      drawing.pop();
+    }
+
+    let str = { shape: drawing };
+    const name = "drawing-" + new Date();
+    const writer = createWriter(`${name}.json`);
+    writer.print(JSON.stringify(str));
+    writer.close();
+    writer.clear();
   }
 }
 
-// S to activate Snap mode
 function keyPressed() {
-  if (supArr.length != 0) {
-    if (keyCode == 83) {
-      snap = true;
-    }
+  // Hold "S" to activate Snap mode
+  if (keyCode == 83) snap = true;
+}
+
+// Using simple (x,y) obj insetad of createVector for better performance.
+class P {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
   }
 }
